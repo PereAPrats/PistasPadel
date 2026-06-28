0f32ec52e522b3c429aed28c65d11c0feecf66ff
PereAPrats <ppratsv03@gmail.com>
2026-06-28 11:19:38 +0200

    fix: add REFERENCES privilege to padel_user for foreign keys

--- db/init/01-create-app-user.sql
+++ db/init/01-create-app-user.sql
@@ -1,4 +1,4 @@
 -- Crear usuari de l'aplicació amb permisos per a migracions i operacions CRUD
 CREATE USER IF NOT EXISTS 'padel_user'@'%' IDENTIFIED BY 'padel_pass';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX ON padel.* TO 'padel_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, INDEX, REFERENCES ON padel.* TO 'padel_user'@'%';
 FLUSH PRIVILEGES;
